15077276c3e51f168dc6b19a1bbf90e64818c5c4
danutzcodrescu <danutzcodrescu@gmail.com>
2018-10-05 22:13:01 +0200

    reverted to url search

--- src/func.ts
+++ src/func.ts
@@ -2,7 +2,8 @@ import { scrap } from "./puppeteer";
 
 const req: any = {
   query: {
-    item: "London Rules: Jackson Lamb Thriller 5",
+    url:
+      "https://www.amazon.com/London-Rules-Jackson-Lamb-Thriller-ebook/dp/B072L7JM4K/ref=tmm_kin_swatch_0?_encoding=UTF8&qid=1538671659&sr=8-1",
     min: 4
   }
 };
--- src/puppeteer.ts
+++ src/puppeteer.ts
@@ -6,19 +6,16 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY!);
 let browser: Browser;
 const startBrowser = async () => {
   browser = await puppeteer.launch({
-    args: ["--no-sandbox"]
-    // headless: false
+    args: ["--no-sandbox"],
+    headless: false
   });
   return browser.newPage();
 };
 
 const getElements = async (page: puppeteer.Page) => {
-  await page.waitForSelector(".a-size-base.a-color-price.a-color-price");
+  // await page.waitForSelector(".a-size-base.a-color-price.a-color-price");
   const [element, title] = await Promise.all([
-    page.$eval(
-      ".a-size-base.a-color-price.a-color-price",
-      (elem: any) => elem.innerText
-    ),
+    page.$eval("td.a-color-price", (elem: any) => elem.innerText),
     page.$eval("#ebooksProductTitle", (elem: any) => elem.innerText)
   ]);
   const pattern = new RegExp(/([1-9][0-9]*.[0-9]+)/);
@@ -37,19 +34,20 @@ const sendEmail = async (price: string, title: string, url: string) => {
 };
 
 export const scrap = async (req: Request, res: Response) => {
-  const item = req.query.item;
+  // const item = req.query.item;
+  const url = req.query.url;
   const page = await startBrowser();
-  await page.goto("https://www.amazon.com");
-  await page.type("#twotabsearchtextbox", item);
-  await page.click(".nav-search-submit > input:nth-child(2)");
-  await page.waitForSelector("h2.a-size-medium");
-  page.click("h2.a-size-medium");
-  await page.waitForSelector(".a-size-base.a-color-price.a-color-price");
+  await page.goto(url);
+  // await page.goto("https://www.amazon.com");
+  // await page.type("#twotabsearchtextbox", item);
+  // await page.click(".nav-search-submit > input:nth-child(2)");
+  // await page.waitForSelector("h2.a-size-medium");
+  // page.click("h2.a-size-medium");
 
-  // const { price, title } = await getElements(page);
-  // if (price < req.query.min) {
-  //   await sendEmail(price, title, page.url());
-  // }
-  // await browser.close();
-  res.send(await page.content());
+  const { price, title } = await getElements(page);
+  if (price < req.query.min) {
+    await sendEmail(price, title, page.url());
+  }
+  await browser.close();
+  res.send(`${price < req.query.min} ${title}: ${price}`);
 };
